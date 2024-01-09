ea66ccf19c1cb0cbcd01c8df9aed4cb7bffec4ce
LightThemes <84159573+LightThemes@users.noreply.github.com>
2024-01-08 21:12:56 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 window.onkeyup = keyup;
 var inputTextValue;
 
+function autoOpen() {
+  var item = document.getElementById("searchTxt").value;
+  result = item.toLowerCase();
+  if (result == "twitch") {openURL("https://www.twitch.tv");}
+  else if (result == "youtube") {openURL("https://www.youtube.com/");}
+  else if (result == "osu") {openURL("https://osu.ppy.sh/users/16780466");}
+  else if (result == "rankings") {openURL("https://osu.ppy.sh/rankings/osu/performance");}
+  else if (result == "skinning") {openURL("https://osu.ppy.sh/wiki/en/Skinning");}
+  else if (result == "github") {openURL("https://github.com/LightThemes?tab=repositories");}
+  else if (result == "mal") {openURL("https://myanimelist.net/profile/LightTheme");}
+  else if (result == "aniwave") {openURL("https://aniwave.to/home");}
+  else if (result == "amazon") {openURL("https://www.amazon.com/");}
+  else if (result == "bestbuy") {openURL("https://www.bestbuy.com/");}
+  else if (result == "wiki") {openURL("https://warframe.fandom.com/wiki/WARFRAME_Wiki");}
+  else if (result == "market") {openURL("https://warframe.market/");}
+  else if (result == "tierlist") {openURL("https://overframe.gg/tier-list/melee-weapons/");}
+  else if (result == "xqc") {openURL("https://twitch.tv/xqc");}
+  else if (result == "poke") {openURL("https://twitch.tv/pokelawls");}
+  else if (result == "btmc") {openURL("https://twitch.tv/btmc");}
+  else if (result == "charlie") {openURL("https://www.youtube.com/@penguinz0/videos");}
+  else if (result == "ntts") {openURL("https://www.youtube.com/@NoTextToSpeech/videos");}
+  else if (result == "stew") {openURL("https://www.youtube.com/@sst3w/videos");}
+}
+
 function rafAsync() {
   return new Promise(resolve => {
       requestAnimationFrame(resolve);
